153168a7621f574e084aad83726e730f29b802bb
Lawson <lwoods@tgen.org>
2025-08-25 18:36:24 -0700

    template improvements

--- scripts/slurm-stub.sh
+++ scripts/slurm-stub.sh
@@ -6,10 +6,10 @@
 #SBATCH --mem=64G
 #SBATCH -c 1
 #SBATCH --time=8:00:00
-#SBATCH --output=tmp/nextflow/REPLACEME
+#SBATCH --output=logs/REPLACEME/slurm.%j.log
 
-export NXF_LOG_FILE=tmp/nextflow/REPLACEME
-export NXF_CACHE_DIR=tmp/nextflow/REPLACEME
+export NXF_LOG_FILE=logs/REPLACEME/.nextflow.log
+export NXF_CACHE_DIR=logs/REPLACEME/.nextflow
 
 conda run -n nf-core --live-stream nextflow run \
     REPLACE_ME
